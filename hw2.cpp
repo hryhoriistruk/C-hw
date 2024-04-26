47332d794007934db17735b8118d5ba03b01f5cd
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-04-26 21:36:49 +0300

    Create hw2.cpp

---
+++ hw2.cpp
@@ -0,0 +1,34 @@
+// 1 Напишіть програму, яка відображає повідомлення I love C++.
+// #include <iostream>
+// using namespace std;
+//
+// int main()
+// {
+//     cout << "I love C++";
+//     return 0;
+// }
+// Напишіть найменшу програму, яку можна скомпілювати, скомпонувати та виконати.
+// int main() {
+//     return 0;
+// }
+// 3 Баги: введіть цю програму та скомпілюйте її. Чому вона дає збій? Як це можна виправити?
+//
+//
+//
+// #include <iostream>
+// using namespace std;
+//
+// int main()
+// {
+//     cout << Is there a bug here?"
+//     return 0;
+// }
+// 4. Виправте помилку в вправі 3, після чого перекомпілюйте її, скомпонуйте і запустіть на виконання.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Is there a bug here?";
+    return 0;
+}
